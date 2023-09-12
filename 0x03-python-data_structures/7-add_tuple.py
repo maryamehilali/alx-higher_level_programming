1cc8198f0ecee27868e7cd76bbc624e16ab1c139
Maryame <elhilali.maryame2@gmail.com>
2023-09-12 10:49:27 -0700

    task 7 codestyle

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    
     a1 = 0 if len(tuple_a) == 0 else tuple_a[0]
     a2 = 0 if len(tuple_a) < 2 else tuple_a[1]
     b1 = 0 if len(tuple_b) == 0 else tuple_b[0]
