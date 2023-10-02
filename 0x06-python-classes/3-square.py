b0216cc06a9fe3c5120e066bf5da26318beda59f
Maryame <elhilali.maryame2@gmail.com>
2023-10-02 16:15:57 -0700

    3square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""class that creats squares"""
+
+
+class Square:
+    """This is a class creates a square
+        it gives it the private attribute size
+    """
+    def __init__(self, size=0):
+        """a methode that initialise the square
+
+        _size: private attribute size of the square
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """a methode that returns the area of the square"""
+        return self.__size * self.__size
