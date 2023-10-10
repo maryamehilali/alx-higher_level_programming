59e99774c0a8ccbeb9cb3b9ab434d26d33e735b3
Maryame <elhilali.maryame2@gmail.com>
2023-10-10 07:50:20 -0700

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module that contains a lookup function"""
+
+
+def lookup(obj):
+    """
+        This funtion prints the list of attributes and methods
+        of an object
+    """
+    return dir(obj)
