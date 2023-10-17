9c1adc7403049314f724fefb667e82e7ac27fbc2
maryame <elhilali.maryame2@gmail.com>
2023-10-17 01:28:57 +0100

    tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -7,4 +7,20 @@ import unittest
 
 class TestRectangle(unittest.TestCase):
     """suite tests for Rectangle Class"""
-    
+    def test_rectangle1(self):
+        """test new rectangle instance"""
+        new = Rectangle(1, 1)
+        self.assertEqual(new.width, 1)
+        self.assertEqual(new.height, 1)
+        self.assertEqual(new.x, 0)
+        self.assertEqual(new.y, 0)
+        self.assertEqual(new.id, 1)
+
+    def test_rectangle2(self):
+        """test new rectangle with id"""
+        new = Rectangle(2, 4, 3, 4, 18)
+        self.assertEqual(new.width, 2)
+        self.assertEqual(new.height, 4)
+        self.assertEqual(new.x, 3)
+        self.assertEqual(new.y, 4)
+        self.assertEqual(new.id, 18)
