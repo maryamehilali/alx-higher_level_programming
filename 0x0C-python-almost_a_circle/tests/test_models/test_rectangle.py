c4cccbeedffaf2522c2017f84879cdc8fe813441
maryame <elhilali.maryame2@gmail.com>
2023-10-17 01:22:50 +0100

    tests/

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""tests for the class Rectangle"""
+from models.base import Base
+from models.rectangle import Rectangle
+import unittest
+
+
+class TestRectangle(unittest.TestCase):
+    """suite tests for Rectangle Class"""
+    
